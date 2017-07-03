04a97f5738e8a9f133c2549a0d53490134257516
Alfonso Di Martino <>
2017-07-03 18:31:11 +0200

    modifica tabella dei test

--- Src/LinearRegression.vhd
+++ Src/LinearRegression.vhd
@@ -71,8 +71,10 @@ end LinearRegression;
 --! <img src="../schemes/LinearRegression.jpg"/>
 --! </div>
 --! @endhtmlonly
---!<table cellspacing="10">
---!<tr>
+--!
+--! @test
+--! <table>
+--! <tr>
 --!	<th>Test Case #</th>
 --! 	<th>Componente interessato</th>
 --! 	<th>Obbiettivo</th>
@@ -80,7 +82,7 @@ end LinearRegression;
 --! 	<th>Output ottenuto</th>
 --! 	<th>Output atteso</th>
 --! 	<th>Esito</th>
---! <tr>
+--! </tr>
 --! <tr>
 --! 	<td>1</td>
 --! 	<td>MULT1</td>
@@ -145,7 +147,6 @@ end LinearRegression;
 --! 		P4=		  b" 000000000000010101001100"</td>
 --! 	<td>Superato</td>
 --! </tr>
-
 --! <tr>
 --! 	<td>5</td>
 --! 	<td>MULT1, MULT3, ADD1</td>
@@ -172,7 +173,6 @@ end LinearRegression;
 --! 	</td>
 --! 	<td>Superato</td>
 --! </tr>
-
 --! <tr>
 --! 	<td>6</td>
 --! 	<td>MULT2, MULT4, ADD6</td>
@@ -190,7 +190,6 @@ end LinearRegression;
 --! 		P4=		  b"001010000100110111101100"<br>
 --! 		add2_out= b"001100010101010110111010"<br>
 --! 		S6=       b"000110001010101011011101"
-		
 --! 	</td>
 --! 	<td align="right">
 --! 		mult2_out=b"000001100000100100000111110011100100011000101001"<br>
@@ -202,7 +201,6 @@ end LinearRegression;
 --! 	</td>
 --! 	<td>Superato</td>
 --! </tr>
-
 --! <tr>
 --! 	<td>7</td>
 --! 	<td>MULT1, MULT2, MULT3, MULT4, ADD5, ADD6, MULT5</td>
@@ -246,7 +244,6 @@ end LinearRegression;
 --! 	</td>
 --! 	<td>Superato</td>
 --! </tr>
-
 --! <tr>
 --! 	<td>8</td>
 --! 	<td>MULT1, MULT2, MULT3, MULT4, ADD5, ADD6, MULT6</td>
@@ -291,154 +288,153 @@ end LinearRegression;
 --! 	<td>Superato</td>
 --! </tr>
 --! </table>
-
 architecture Structural of LinearRegression is
 
-component multiplier is
-	Generic (nbits1	   : natural := 8; 	
-			 nbits2	   : natural := 8); 
-    Port ( factor1 : in STD_LOGIC_VECTOR (nbits1-1 downto 0);	
-           factor2 : in STD_LOGIC_VECTOR (nbits2-1 downto 0);	
-           prod : out STD_LOGIC_VECTOR (nbits1+nbits2-1 downto 0));	
-end component;
+	component multiplier is
+		Generic (nbits1	   : natural := 8; 	
+				 nbits2	   : natural := 8); 
+		Port ( factor1 : in STD_LOGIC_VECTOR (nbits1-1 downto 0);	
+			   factor2 : in STD_LOGIC_VECTOR (nbits2-1 downto 0);	
+			   prod : out STD_LOGIC_VECTOR (nbits1+nbits2-1 downto 0));	
+	end component;
 
 
-component adder is
-	generic (	nbits 		: 		natural := 32;
-				use_custom 	:		boolean := false);	
-	port (		add1 		: in	std_logic_vector(nbits-1 downto 0);		
-				add2 		: in	std_logic_vector(nbits-1 downto 0);		
-				sum			: out	std_logic_vector(nbits-1 downto 0);		
-				overflow	: out	std_logic);	
-end component;
+	component adder is
+		generic (	nbits 		: 		natural := 32;
+					use_custom 	:		boolean := false);	
+		port (		add1 		: in	std_logic_vector(nbits-1 downto 0);		
+					add2 		: in	std_logic_vector(nbits-1 downto 0);		
+					sum			: out	std_logic_vector(nbits-1 downto 0);		
+					overflow	: out	std_logic);	
+	end component;
 
-signal mult1_out : std_logic_vector (28 downto 0):=(others=>'0'); --! Uscita di MULT1 espressa su 29 bit, di cui 8 per 
---! la parte intera e 21 per quella decimale (m.n = 7.21).
-signal P3 : std_logic_vector (23 downto 0):=(others=>'0'); --!L'uscita di MULT1 deve essere espressa su 24 bit
---! di cui 5 sono per la parte intera, e 19 per quella decimale (m.n = 4.19).	
-
-
-
-signal mult2_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT2 espressa su 48 bit, di cui 6 per 
---! la parte intera e 42 per quella decimale (m.n = 5.42).
-signal P2 : std_logic_vector (23 downto 0):=(others=>'0'); --!L'uscita di MULT2 deve essere espressa su 24 bit
---! in cui l'msb ha peso -3 (m.n = -3.26).		
-
-signal mult3_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT3 espressa su 48 bit, di cui 11 per 
---! la parte intera e 37 per quella decimale (m.n = 10.37).
-signal P1 : std_logic_vector (23 downto 0):=(others=>'0'); --! L'uscita di MULT3 deve essere espressa su 24 bit,
---! di cui 5 bit per la parte intera e 19 per quella decimale ( m.n = 4.19 ).
-
-signal mult4_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT4 espressa su 48 bit, in cui l'msb ha
---! peso pari a -2  (m.n = -2.49).
-signal P4 : std_logic_vector (23 downto 0):=(others=>'0'); --! L'uscita di MULT4 deve essere espressa su 24 bit,
---! in cui l'msb ha peso -3 ( m.n = -3.26 ).
+	signal mult1_out : std_logic_vector (28 downto 0):=(others=>'0'); --! Uscita di MULT1 espressa su 29 bit, di cui 8 per 
+	--! la parte intera e 21 per quella decimale (m.n = 7.21).
+	signal P3 : std_logic_vector (23 downto 0):=(others=>'0'); --!L'uscita di MULT1 deve essere espressa su 24 bit
+	--! di cui 5 sono per la parte intera, e 19 per quella decimale (m.n = 4.19).	
 
 
-signal S5 : std_logic_vector (23 downto 0):=(others=>'0'); --! L'uscita di ADD1 deve essere espressa su 24 bit,
---! di cui 5 per la parte intera  e 19 decimale ( m.n = 4.19 ).
+
+	signal mult2_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT2 espressa su 48 bit, di cui 6 per 
+	--! la parte intera e 42 per quella decimale (m.n = 5.42).
+	signal P2 : std_logic_vector (23 downto 0):=(others=>'0'); --!L'uscita di MULT2 deve essere espressa su 24 bit
+	--! in cui l'msb ha peso -3 (m.n = -3.26).		
+
+	signal mult3_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT3 espressa su 48 bit, di cui 11 per 
+	--! la parte intera e 37 per quella decimale (m.n = 10.37).
+	signal P1 : std_logic_vector (23 downto 0):=(others=>'0'); --! L'uscita di MULT3 deve essere espressa su 24 bit,
+	--! di cui 5 bit per la parte intera e 19 per quella decimale ( m.n = 4.19 ).
+
+	signal mult4_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT4 espressa su 48 bit, in cui l'msb ha
+	--! peso pari a -2  (m.n = -2.49).
+	signal P4 : std_logic_vector (23 downto 0):=(others=>'0'); --! L'uscita di MULT4 deve essere espressa su 24 bit,
+	--! in cui l'msb ha peso -3 ( m.n = -3.26 ).
 
 
-signal add2_out : std_logic_vector (23 downto 0):=(others=>'0'); --! Uscita di ADD2 espressa su 48 bit, in cui l'msb ha
---! peso pari a -3  (m.n = -3.26).
-signal S6 : std_logic_vector (23 downto 0):=(others=>'0'); --! L'uscita di ADD2 deve essere espressa su 24 bit,
---! in cui l'msb ha peso -3 ( m.n = -2.25 ).
-				  
-signal mult5_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT2 espressa su 48 bit, di cui 21 per 
---! la parte intera e 27 per quella decimale (m.n = 20.27).
+	signal S5 : std_logic_vector (23 downto 0):=(others=>'0'); --! L'uscita di ADD1 deve essere espressa su 24 bit,
+	--! di cui 5 per la parte intera  e 19 decimale ( m.n = 4.19 ).
 
-signal mult6_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT2 espressa su 48 bit, di cui 15 per 
---! la parte intera e 33 per quella decimale (m.n = 14.330).
+
+	signal add2_out : std_logic_vector (23 downto 0):=(others=>'0'); --! Uscita di ADD2 espressa su 48 bit, in cui l'msb ha
+	--! peso pari a -3  (m.n = -3.26).
+	signal S6 : std_logic_vector (23 downto 0):=(others=>'0'); --! L'uscita di ADD2 deve essere espressa su 24 bit,
+	--! in cui l'msb ha peso -3 ( m.n = -2.25 ).
+					  
+	signal mult5_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT2 espressa su 48 bit, di cui 21 per 
+	--! la parte intera e 27 per quella decimale (m.n = 20.27).
+
+	signal mult6_out : std_logic_vector (47 downto 0):=(others=>'0'); --! Uscita di MULT2 espressa su 48 bit, di cui 15 per 
+	--! la parte intera e 33 per quella decimale (m.n = 14.330).
 
 begin
 
-MULT1: multiplier
-	Generic map( nbits1 => 5,
-				 nbits2 => 24)
-	port map ( factor1 => prim,
-			   factor2 => Sum2,
-			   prod => mult1_out);
-			   
-P3<= mult1_out(25 downto 2);  --! Cambio di rappresentazione dell'uscita di MULT1 da 29 bit, di cui 8 per la parte intera 
---! (m.n = 7.21) a 24 bit, di cui 5 per la parte intera (m.n = 4.19). Quindi tronchiamo 3 bit in testa e 2 in coda.
+	MULT1: multiplier
+		Generic map( nbits1 => 5,
+					 nbits2 => 24)
+		port map ( factor1 => prim,
+				   factor2 => Sum2,
+				   prod => mult1_out);
+				   
+	P3<= mult1_out(25 downto 2);  --! Cambio di rappresentazione dell'uscita di MULT1 da 29 bit, di cui 8 per la parte intera 
+	--! (m.n = 7.21) a 24 bit, di cui 5 per la parte intera (m.n = 4.19). Quindi tronchiamo 3 bit in testa e 2 in coda.
 
 
-MULT2: multiplier
-	Generic map( nbits1 => 24,
-				 nbits2 => 24)
-	port map ( factor1 => Sum2,
-			   factor2 => B,
-			   prod => mult2_out);
-			   
-P2<= mult2_out(39 downto 16);  --! Cambio di rappresentazione dell'uscita di MULT2 da 48 bit, di cui 6 per la parte intera 
---! (m.n = 5.42) a 24 bit, in cui l'msb ha peso -3 (m.n = -3.26). Quindi tronchiamo 8 bit in testa e 16 in coda.
+	MULT2: multiplier
+		Generic map( nbits1 => 24,
+					 nbits2 => 24)
+		port map ( factor1 => Sum2,
+				   factor2 => B,
+				   prod => mult2_out);
+				   
+	P2<= mult2_out(39 downto 16);  --! Cambio di rappresentazione dell'uscita di MULT2 da 48 bit, di cui 6 per la parte intera 
+	--! (m.n = 5.42) a 24 bit, in cui l'msb ha peso -3 (m.n = -3.26). Quindi tronchiamo 8 bit in testa e 16 in coda.
 
 
-MULT3: multiplier
-	Generic map( nbits1 => 24,
-				 nbits2 => 24)
-	port map ( factor1 => B,
-			   factor2 => Sum1,
-			   prod => mult3_out);
-			   
-P1<= mult3_out(41 downto 18);  --! Cambio di rappresentazione dell'uscita di MULT3 da 48 bit, di cui 11 per la parte intera 
---! (m.n = 10.37) a 24 bit, di cui 5 per la parte intera (m.n = 4.19). Quindi tronchiamo 6 bit in testa e 18 in coda.
+	MULT3: multiplier
+		Generic map( nbits1 => 24,
+					 nbits2 => 24)
+		port map ( factor1 => B,
+				   factor2 => Sum1,
+				   prod => mult3_out);
+				   
+	P1<= mult3_out(41 downto 18);  --! Cambio di rappresentazione dell'uscita di MULT3 da 48 bit, di cui 11 per la parte intera 
+	--! (m.n = 10.37) a 24 bit, di cui 5 per la parte intera (m.n = 4.19). Quindi tronchiamo 6 bit in testa e 18 in coda.
 
 
-MULT4: multiplier
-	Generic map( nbits1 => 24,
-				 nbits2 => 24)
-	port map ( factor1 => Sum1,
-			   factor2 => C,
-			   prod => mult4_out);
-			   
-P4<= mult4_out(46 downto 23);  --! Cambio di rappresentazione dell'uscita di MULT4 da 48 bit, in cui l'msb ha peso -2 (m.n = -2.49),
---! a 24 bit, in cui l'msb ha peso -3 (m.n = -3.26). Quindi tronchiamo 1 bit in testa e 23 in coda.
+	MULT4: multiplier
+		Generic map( nbits1 => 24,
+					 nbits2 => 24)
+		port map ( factor1 => Sum1,
+				   factor2 => C,
+				   prod => mult4_out);
+				   
+	P4<= mult4_out(46 downto 23);  --! Cambio di rappresentazione dell'uscita di MULT4 da 48 bit, in cui l'msb ha peso -2 (m.n = -2.49),
+	--! a 24 bit, in cui l'msb ha peso -3 (m.n = -3.26). Quindi tronchiamo 1 bit in testa e 23 in coda.
 
-ADD1: adder
-	Generic map (nbits => 24,
-				 use_custom => false)
-	port map( add1 => P3,
-			  add2 => P1,
-			  sum => S5,
-			  overflow => open);
-			  
-			  
-ADD2: adder
-	Generic map (nbits => 24,
-				 use_custom => false)
-	port map( add1 => P2,
-			  add2 => P4,
-			  sum => add2_out,
-			  overflow => open);
-			  
-S6 <= add2_out(23) & add2_out (23 downto 1);  --! L'uscita di ADD2 deve essere portata da una rappresentazione a 24 bit con peso
---! dell'msb pari a -3 (m.n = -3.26) a una con 24 bit con peso dell'msb pari a -2 (m.n = -2.25). Quindi tronchiamo un bit in coda ed aggiungiamo
---! un bit in testa con estensione del segno.
+	ADD1: adder
+		Generic map (nbits => 24,
+					 use_custom => false)
+		port map( add1 => P3,
+				  add2 => P1,
+				  sum => S5,
+				  overflow => open);
+				  
+				  
+	ADD2: adder
+		Generic map (nbits => 24,
+					 use_custom => false)
+		port map( add1 => P2,
+				  add2 => P4,
+				  sum => add2_out,
+				  overflow => open);
+				  
+	S6 <= add2_out(23) & add2_out (23 downto 1);  --! L'uscita di ADD2 deve essere portata da una rappresentazione a 24 bit con peso
+	--! dell'msb pari a -3 (m.n = -3.26) a una con 24 bit con peso dell'msb pari a -2 (m.n = -2.25). Quindi tronchiamo un bit in coda ed aggiungiamo
+	--! un bit in testa con estensione del segno.
 
 
-MULT5: multiplier
-	Generic map( nbits1 => 24,
-				 nbits2 => 24)
-	port map ( factor1 => S5,
-			   factor2 => A,
-			   prod => mult5_out);
-			   
-m <= mult5_out (42 downto 19); --! L'uscita di  MULT5 deve essere portata da una rappresentazione di 48 bit con
---! 21 bit di parte intera e 27 decimale (m.n = 20.27), ad una di 24 bit con 16 bit di parte intera e 8 decimale (m.n = 15.8). 
---! Qindi tronca 5 bit in testa e 19 in coda.		
-			   
-MULT6: multiplier
-	Generic map( nbits1 => 24,
-				 nbits2 => 24)
-	port map ( factor1 => A,
-			   factor2 => S6,
-			   prod => mult6_out);	  			  
-	
-			  
-q <= mult6_out(40 downto 17);	--! L'uscita di  MULT6 deve essere portata da una rappresentazione di 48 bit con
---! 15 bit di parte intera e 33 decimale (m.n = 14.33), ad una di 24 bit con 8 bit di parte intera e 16 decimale (m.n = 7.16). 
---! Qindi tronca 7 bit in testa e 17 in coda.
+	MULT5: multiplier
+		Generic map( nbits1 => 24,
+					 nbits2 => 24)
+		port map ( factor1 => S5,
+				   factor2 => A,
+				   prod => mult5_out);
+				   
+	m <= mult5_out (42 downto 19); --! L'uscita di  MULT5 deve essere portata da una rappresentazione di 48 bit con
+	--! 21 bit di parte intera e 27 decimale (m.n = 20.27), ad una di 24 bit con 16 bit di parte intera e 8 decimale (m.n = 15.8). 
+	--! Qindi tronca 5 bit in testa e 19 in coda.		
+				   
+	MULT6: multiplier
+		Generic map( nbits1 => 24,
+					 nbits2 => 24)
+		port map ( factor1 => A,
+				   factor2 => S6,
+				   prod => mult6_out);	  			  
+		
+				  
+	q <= mult6_out(40 downto 17);	--! L'uscita di  MULT6 deve essere portata da una rappresentazione di 48 bit con
+	--! 15 bit di parte intera e 33 decimale (m.n = 14.33), ad una di 24 bit con 8 bit di parte intera e 16 decimale (m.n = 7.16). 
+	--! Qindi tronca 7 bit in testa e 17 in coda.
 
 end Structural;
 
