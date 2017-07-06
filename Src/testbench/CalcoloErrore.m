6b84693a02e79b9efa99f9c1fcc9c2afcf182eef
Liguori <pie.liguori@gmail.com>
2017-07-06 17:29:08 +0200

    test matlab

--- Src/testbench/CalcoloErrore.m
+++ Src/testbench/CalcoloErrore.m
@@ -1,16 +1,16 @@
 dataset=textread('dataset.txt','%s');
 m_matlab_bin= dataset(3:4:end);
 q_matlab_bin= dataset(4:4:end);
-m_matlab=bin2num(quantizer([24 14]),m_matlab_bin);
-q_matlab=bin2num(quantizer([24 23]),q_matlab_bin);
+m_matlab=bin2num(quantizer([24 13]),m_matlab_bin);
+q_matlab=bin2num(quantizer([24 21]),q_matlab_bin);
 m_matlab=cell2mat(m_matlab);
 q_matlab=cell2mat(q_matlab);
 
 out=textread('output.txt', '%s');
 m_vivado_bin =  out(1:2:end);
 q_vivado_bin =  out(2:2:end);
-m_vivado=bin2num(quantizer([24 14]),m_vivado_bin);
-q_vivado=bin2num(quantizer([24 23]),q_vivado_bin);
+m_vivado=bin2num(quantizer([24 13]),m_vivado_bin);
+q_vivado=bin2num(quantizer([24 21]),q_vivado_bin);
 m_vivado=cell2mat(m_vivado);
 q_vivado=cell2mat(q_vivado);
 
--- Src/testbench/TBgenerator.m
+++ Src/testbench/TBgenerator.m
@@ -8,7 +8,7 @@ t_fast = (0:1/fs:T_P-1/fs); % fast time axis [s]
 myFile='dataset.txt';       % Filename
     fid = fopen(myFile,'w');        % Open file
 
-for j=1:1000
+for j=1:10000
     
     sig=zeros(1,n);
     sig(1)=rand()-0.5; % This signal simulates the output of an atan2 module
@@ -29,18 +29,32 @@ for j=1:1000
     % Compute operations
     s1=sum(sig);
     s2=sum(x.*sig);
-
     m = (n*s2 - s1*sx) *div1;
     q = (s1*sx2 - sx*s2) *div1;
+    
+    
+%     m(j,1) = (n*s2 - s1*sx) *div1;
+%     q (j,1)= (s1*sx2 - sx*s2) *div1;
+%     sum1(j,1)=s1;
+%     sum2(j,1)=s2;
+%     
+%     mult3(j,1)= n*s2;
+%     mult1(j,1)= s1*sx;
+%     
+%     mult2(j,1)=sx*s2;
+%     mult4(j,1)=s1*sx2;
+%     
+%     sub5(j,1)=n*s2 - s1*sx;
+%     sub6(j,1)=s1*sx2 - sx*s2;
 
 
     % %% Write data on file
     
-    s1_bin = num2bin(quantizer([24 16]),s1); % Generate binary string
-    s2_bin = num2bin(quantizer([24 21]),s2);
-    m_bin = num2bin(quantizer([24 14]),m);
-    q_bin = num2bin(quantizer([24 23]),q); 
-    fwrite(fid,[s1_bin char(32) s2_bin char(32) m_bin char(32) q_bin char(32) char(13) char(10)]); % Remember to add \r\n to end lines!
+     s1_bin = num2bin(quantizer([24 15]),s1); % Generate binary string
+     s2_bin = num2bin(quantizer([24 21]),s2);
+     m_bin = num2bin(quantizer([24 13]),m);
+     q_bin = num2bin(quantizer([24 21]),q); 
+     fwrite(fid,[s1_bin char(32) s2_bin char(32) m_bin char(32) q_bin char(32) char(13) char(10)]); % Remember to add \r\n to end lines!
 
                   
 end
